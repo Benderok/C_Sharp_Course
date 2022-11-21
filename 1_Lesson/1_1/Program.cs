68b3e6365f6445eb26b523142d499cd732a60575
Valentin <vill451@mail.ru>
2022-11-22 00:54:13 +0600

    added excersise 1_1

---
+++ 1_Lesson/1_1/Program.cs
@@ -0,0 +1,16 @@
+﻿// 1. Напишите программу, которая на вход принимает два числа и проверяет,
+//    является ли первое число квадатом второго
+
+Console.WriteLine("Enter number 1");
+int num1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter number 2");
+int num2 = int.Parse(Console.ReadLine());
+
+if(num2 == num1*num1)
+{
+    Console.WriteLine("Yes");
+}
+
+else 
+    Console.WriteLine("No");
