1da54527ec579a4288424b6d8e0b1b08a6ebafc2
Valentin <vill451@mail.ru>
2022-11-23 17:51:55 +0600

    Correct version of Task 1 pfmd

--- HomeWork/Lesson_1/Program.cs
+++ HomeWork/Lesson_1/Program.cs
@@ -83,8 +83,28 @@
 
 //Напишите программу которая, на вход принимает число и выдает его квадрат.
 
-Console.WriteLine("Enter number to get its double:");
+//Console.WriteLine("Enter number to get its double:");
+//var x = Console.ReadLine();
+//int N = x == null ? 0 : int.Parse(x);
+//Console.Write($"Double of {N} is: ");
+//Console.WriteLine(N * N);
+
+// Напишите программу, которая на вход принимает 2 числа и выдает, какое число больше, а какое меньше.
+
+Console.WriteLine("Enter number 1:");
 var x = Console.ReadLine();
-int N = x == null ? 0 : int.Parse(x);
-Console.Write($"Double of {N} is: ");
-Console.WriteLine(N * N);
+Console.WriteLine("Enter number 2:");
+var y = Console.ReadLine();
+int A = int.Parse(x);
+int B = int.Parse(y);
+int Max = A;
+if (B > A)
+{
+    Max = B;
+}
+else
+{
+    Console.WriteLine(Max);
+}
+Console.WriteLine($"Maximum of {A} and {B} is: ");
+Console.WriteLine(Max);
