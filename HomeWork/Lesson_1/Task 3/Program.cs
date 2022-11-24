247cfae9e74c7cc951ee73481fd7a79a47271597
Valentin <vill451@mail.ru>
2022-11-24 16:50:15 +0600

    Taks 3 pfmd

---
+++ HomeWork/Lesson_1/Task 3/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+Console.WriteLine("Enter number to know if is even or odd.");
+Console.WriteLine("Enter number: ");
+var a = Console.ReadLine();
+int A = int.Parse(a);
+if (A % 2 == 0)
+{
+    Console.WriteLine("Even");
+}
+else
+{
+    Console.WriteLine("Odd");
+}
