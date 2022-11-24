85b72eca575d406e63e45a915da663f967d84dc8
Valentin <vill451@mail.ru>
2022-11-24 16:39:12 +0600

    Taks 2 pfmd

--- HomeWork/Lesson_1/Task 2/Program.cs
+++ HomeWork/Lesson_1/Task 2/Program.cs
@@ -1,3 +1,23 @@
 ﻿// Напишите программу, которая принимает на вход 3 числа и выдает максимальное из этих чисел.
 
-
+Console.WriteLine("Enter 3 numbers to know the maximum of it.");
+Console.WriteLine("Enter number 1:");
+var a = Console.ReadLine();
+Console.WriteLine("Enter number 2:");
+var b = Console.ReadLine();
+Console.WriteLine("Enter number 3:");
+var c = Console.ReadLine();
+int A = int.Parse(a);
+int B = int.Parse(b);
+int C = int.Parse(c);
+int Max = A;
+if (B > Max)
+{
+    Max = B;
+}
+if (C > Max)
+{
+    Max = C;
+}
+Console.Write($"The maximum is ");
+Console.Write(Max);
