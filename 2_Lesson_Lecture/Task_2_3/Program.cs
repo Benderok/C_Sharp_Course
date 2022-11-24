9c452fa0823e9fd9e095fa6ed492fb96707c5f1f
Valentin <vill451@mail.ru>
2022-11-24 20:46:35 +0600

    Tasks from Lecture added

---
+++ 2_Lesson_Lecture/Task_2_3/Program.cs
@@ -0,0 +1,14 @@
+﻿int[] array = {186, 27, 3786, 41, 545, 64, 724, 18};
+int n = array.Length;
+int find = 18;
+int index = 0;
+
+while (index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+    
+    }
+    index++;
+}
