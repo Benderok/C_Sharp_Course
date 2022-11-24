4bc0a585dd6df722abddf2ca27e3d922281cc157
Valentin <vill451@mail.ru>
2022-11-24 17:17:34 +0600

    Taks 4 pfmd

---
+++ HomeWork/Lesson_1/Task 4/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 2 до N.
+
+Console.WriteLine("Enter number: ");
+var a = Console.ReadLine();
+int A = int.Parse(a);
+Console.WriteLine("Result:");
+for (int N = 2; N <= A; N += 2)
+{
+    Console.Write(N);
+    if (N == A)
+        continue;
+    Console.Write(",");
+}
