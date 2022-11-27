8cb33637a655322af8e21dec366c356963478486
Valentin <vill451@mail.ru>
2022-11-28 01:09:56 +0600

    HW Task 3 pfmd

---
+++ HomeWork/Lesson_2/Task 3/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает,
+// что третьей цифры нет.
+
+int item = int.Parse(Console.ReadLine());
+
+if (item < 100)
+{
+    Console.WriteLine("Wrong number!");
+    return;
+}
+
+while (item >= 1000)
+{
+    item /= 10;
+}
+Console.WriteLine(item % 10);
