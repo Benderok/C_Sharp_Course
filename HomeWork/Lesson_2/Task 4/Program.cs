345c2922e6ec68a4ccaa51696b6a1849b69a265f
Valentin <vill451@mail.ru>
2022-11-28 01:34:14 +0600

    HW Task 4 pfmd

---
+++ HomeWork/Lesson_2/Task 4/Program.cs
@@ -0,0 +1,31 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели,
+// и проверяет, является ли этот день выходным.
+
+char week = char.Parse(Console.ReadLine());
+switch (week)
+{
+    case '1':
+    Console.WriteLine("No");
+    break;
+    case '2':
+    Console.WriteLine("No");
+    break;
+    case '3':
+    Console.WriteLine("No");
+    break;
+    case '4':
+    Console.WriteLine("No");
+    break;
+    case '5':
+    Console.WriteLine("No");
+    break;
+    case '6':
+    Console.WriteLine("Yes");
+    break;
+    case '7':
+    Console.WriteLine("Yes");
+    break;
+    default:
+    Console.WriteLine("Not a day number");
+    break;
+}
