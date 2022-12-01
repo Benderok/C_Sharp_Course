982e3fd70b6ea4a8346ffb96257b116a3724dab3
Valentin <vill451@mail.ru>
2022-12-01 18:12:13 +0600

    All 3 Tasks from HW3 pfmd

---
+++ HomeWork/Lesson 3/Task 3/Program.cs
@@ -0,0 +1,34 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+// int ReadNumber(string message)
+// {
+//     Console.Write(message);
+//     var s = Console.ReadLine();
+//     int a = s == null ? 0 : int.Parse(s);
+//     return a;
+// }
+
+void Cube(int N)
+{
+   int i = 0;
+   double cube;
+   while (i < N)
+   {
+        i += 1;
+        if (i == (N))
+        {
+            cube = Math.Pow(i, 3);
+            Console.Write($"{cube}.");
+        }
+        else
+        {
+        cube = Math.Pow(i, 3);
+        Console.Write($"{cube}, ");
+        }
+   } 
+}
+
+Console.WriteLine("Enter number: ");
+int x = int.Parse(Console.ReadLine());
+// int x = ReadNumber("Enter number: ");
+Cube(x);
