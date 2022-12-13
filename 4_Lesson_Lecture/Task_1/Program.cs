fc21bf7addcbeb5e01a7330c2ff52a45e71c0249
Valentin Panferin <valentinpanferin@MacBook-Pro-Valentin.local>
2022-12-14 00:44:14 +0700

    4_th_lecture

---
+++ 4_Lesson_Lecture/Task_1/Program.cs
@@ -0,0 +1,42 @@
+﻿// Многомерные массивы
+
+// string[,] table = new string[2, 5];
+// table[1, 2] = "word";
+
+// for (int i = 0; i < 2; i++)
+// {
+//     for (int j = 0; j < 5; j++)
+//     {
+//         Console.WriteLine($"-{table[i, j]}-");
+//     }
+// }
+
+
+void PrinArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+int[,] matrix = new int[3, 4];
+PrinArray(matrix);
+FillArray(matrix);
+Console.WriteLine();
+PrinArray(matrix);
