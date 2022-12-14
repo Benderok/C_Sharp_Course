46e46f0ee7178666002c8bde51077327a86312b8
Valentin Panferin <valentinpanferin@MacBook-Pro-Valentin.local>
2022-12-14 23:03:42 +0700

    Lesson 6 added info

--- 6_lesson/6_3/Program.cs
+++ 6_lesson/6_3/Program.cs
@@ -1,2 +1,15 @@
 ﻿// Выведите первые N чисел Фибоначчи.
 
+void Fibonacci(int n)
+{
+    int a = 0, b = 1;
+
+    for (int i = 0; i < n; i++)
+    {
+        Console.Write($"{a}, ");
+        (a, b) = (b, a + b);
+    }
+}
+
+int n = int.Parse(Console.ReadLine());
+Fibonacci(n);
