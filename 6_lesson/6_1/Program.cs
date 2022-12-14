0334e539022140bd6ca29e58ac0befaa406db79d
Valentin Panferin <valentinpanferin@MacBook-Pro-Valentin.local>
2022-12-14 20:39:26 +0700

    6 th lesson

---
+++ 6_lesson/6_1/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите прорамму, которая принимает на вход 3 числа и проверят,
+// может ли существовать треугольник со сторонами такой длины.
+
+void Triangle()
+{
+    int a = int.Parse(Console.ReadLine());
+    int b = int.Parse(Console.ReadLine());
+    int c = int.Parse(Console.ReadLine());
+
+    if (a < b + c & b < a + c & c < a + b)
+    Console.WriteLine("Yes");
+    else Console.WriteLine("NO");
+}
+Triangle();
